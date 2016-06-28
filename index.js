3e151e620db153653e9d0978ae4a6255489b96cc
Devin Matthews <devin.matthews@carestream.com>
2016-06-27 21:14:08 -0500

    Prep for fetch. Added minor tweaks.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var colors = require('colors');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-//var UserController = require('./src/controllers/user');
+var UserController = require('./src/controllers/user');
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
@@ -11,8 +11,8 @@ var app = new express();
 app.use(bodyParser.json());
 mongoose.connect('mongodb://dm:dm@127.0.0.1/calcDB');
 
-//app.get('/users', UserController.findAll);
-//app.get('/user/name/:name', UserController.findByName);
+app.get('/users', UserController.findAll);
+app.get('/user/name/:name', UserController.findByName);
 
 app.listen(3000, function() {
     console.log('express Listening...'.yellow);
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
 export default class App extends React.Component{
+    constructor (props, state) {
+        super(props, state);
+
+        this.state = {
+            color: 'black'
+        }
+
+        this.getAllUsers = this.getAllUsers.bind(this);
+    }
+
+    shouldComponentUpdate () {
+        //return this.scuKey
+    }
+
+    getAllUsers () {
+        this.setState({
+            color: 'green'
+        });
+    }
+    
     render() {
         return <div>
-            HI!
+            <input style={{backgroundColor: this.state.color}} id="getUsers" type="button" name="getUsers" value="Get All Users"
+                onClick={this.getAllUsers} scuKey={this.name + '_' + this.id} />
         </div>
     }
 }
